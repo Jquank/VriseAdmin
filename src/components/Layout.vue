d38f9838a9d2d76e691a7b29b0453360767e3ca7
gongjian <478197650@qq.com>
2022-03-24 20:09:28 +0800

    add logo

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,38 +1,85 @@
 <template>
     <div class="layout">
         <div class="layout-left">
+            <div class="logo">
+                <img class="logo-img" src="../assets/logo.png" alt="logo" />
+                <transition name="logotext">
+                    <span v-show="showLogoText" class="logo-text">VriseAdmin</span>
+                </transition>
+            </div>
             <MyNav :mode="navMode"></MyNav>
         </div>
         <div class="layout-right">
             <MyHeader></MyHeader>
-            <main>
-                <router-view></router-view>
-            </main>
+            <section class="main-box">
+                <el-scrollbar>
+                    <router-view></router-view>
+                </el-scrollbar>
+            </section>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
     import type { NavMode } from './type'
-    import { ref } from 'vue'
+    import { ref, computed } from 'vue'
+    import { useStore } from '@/store/index'
     import MyHeader from './MyHeader.vue'
     import MyNav from './MyNav.vue'
-
+    const store = useStore()
     const navMode = ref<NavMode>('vertical')
+    const showLogoText = computed(() => {
+        return !store.state.collapse
+    })
 </script>
 
 <style lang="less" scoped>
     .layout {
         display: flex;
         height: 100%;
+        width: 100%;
         .layout-left {
-            // width: 200px;
+            display: flex;
+            flex-direction: column;
             height: 100%;
             background-color: var(--nav-bg-color);
+            .logo {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: var(--nav-item-height);
+                font-size: 18px;
+                font-weight: bold;
+                color: var(--nav-hover-color);
+                overflow: hidden;
+                .logo-img {
+                    width: 20px;
+                }
+                .logo-text {
+                    padding-left: 10px;
+                }
+                .logotext-enter-active,
+                .logotext-leave-active {
+                    transition: all 0.5s cubic-bezier(0.32, 0, 1, 0);
+                }
+                .logotext-enter-from,
+                .logotext-leave-to {
+                    transform: translateX(20px);
+                    width: 0;
+                    opacity: 0;
+                }
+            }
+            .el-scrollbar {
+                flex: 1;
+            }
         }
         .layout-right {
             flex: 1;
             height: 100%;
+            .main-box {
+                width: 100%;
+                overflow: hidden;
+            }
         }
     }
 </style>
--- src/views/systemManage/RoleManage.vue
+++ src/views/systemManage/RoleManage.vue
@@ -1,22 +1,9 @@
 <template>
     <div class="role-manage">
-        <div @click="handleClick">456</div>
-        <el-table :data="tableData">
-            <el-table-column></el-table-column>
-        </el-table>
+        <div>456</div>
     </div>
 </template>
 
-<script setup lang="ts">
-    import { reactive } from 'vue'
-    const tableData = reactive([])
-    const body = document.documentElement
-    const handleClick = () => {
-        console.log(123)
-
-        // body.style.setProperty('--nav-bg-color', 'blue')
-        body.setAttribute('style', '--nav-bg-color: blue')
-    }
-</script>
+<script setup lang="ts"></script>
 
 <style lang="less" scoped></style>
